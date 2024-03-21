a7bdee59c59526672898f5913578d4c7b1cad538
1AW3 User 2 <1AW3-2@zornotzalh.eus>
2024-03-21 14:01:46 +0100

    Ibai 2, commit para subir al github

--- CompuertasAgua/src/base/ControlCompuertas.java
+++ CompuertasAgua/src/base/ControlCompuertas.java
@@ -1,6 +1,10 @@
 package base;
 
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ControlCompuertas {
@@ -14,9 +18,10 @@ public class ControlCompuertas {
 
 	public static void main(String[] args) {
 
-		System.out.println(
-				"Este programa lee el nivel de agua de una presa y permite abrir compuertas si tenemos permiso (el nivel es superior a 50) y las compuertas están verificadas.");
+		System.out.println("Este programa lee el nivel de agua de una presa y permite abrir compuertas si tenemos permiso (el nivel es superior a 50) y las compuertas estan verificadas.");
+		
 
+		
 		int nivel = leerNivelAgua();
 
 		mostrarMenu(nivel);
@@ -24,9 +29,35 @@ public class ControlCompuertas {
 	}
 
 	
-	
+	/**
+	 * En este metodo tenemos todo el texto y menu del programa. dentro hay un do while 
+	 * que contiene el menu y un switch con varios case.
+	 * 
+	 * 
+	 * @param nivel recibe un integer de otro metodo.
+	 * @return No devuelve nada como tal porque es un void en el que tenemos un do while y un switch con varios case.
+	 * @author Asier Iza
+	 */
 	public static void mostrarMenu(int nivel) {
 		int opcion = 0;
+		
+		LOGGER.setUseParentHandlers(false);
+		LOGGER.setLevel(Level.FINE);
+
+		Handler fileHandler = null;
+		try {
+			fileHandler = new FileHandler("./opcionesMenu.log", true);
+			fileHandler.setLevel(Level.FINE);
+			LOGGER.addHandler(fileHandler);
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 		do {
 			System.out.println();
 			System.out.println("Nivel del agua: " + nivel);
@@ -39,22 +70,25 @@ public class ControlCompuertas {
 			System.out.println("	4. Verificar compuertas, estado: " + (compuertasVerificadas ? "VERIFICADAS" : "NO VERIFICADAS"));
 			System.out.println("5. Salir");
 			System.out.println();
-			System.out.print("Introduce opción: ");
+			System.out.print("Introduce opcion: ");
 			opcion = teclado.nextInt();
+			
 			switch (opcion) {
 			case 1:
 				nivel = leerNivelAgua();
 				permiso = false;
 				compuertasVerificadas = false;
+				LOGGER.log(Level.FINE,"1");
 				break;
 			case 2:
 				if(abrirCompuertas()) {
 					System.out.println();
-					System.out.print("¡Compuertas abiertas!");
+					System.out.print("Compuertas abiertas");
 				}else {
 					System.out.println();
 					System.out.print("No se cumplen las condiciones para abrir compuertas.");
 				}
+				LOGGER.log(Level.FINE,"2");
 				break;
 			case 3:
 				permiso = solicitarPermiso(nivel);
@@ -62,13 +96,15 @@ public class ControlCompuertas {
 					System.out.println();
 					System.out.print("El permiso solamente se concede si el nivel del agua es superior a 60.");
 				}
+				LOGGER.log(Level.FINE,"3");
 				break;	
 			case 4:
 				compuertasVerificadas = verificarCompuertas();
 				if(compuertasVerificadas) {
 					System.out.println();
-					System.out.print("¡Compuertas verificadas!");
+					System.out.print("Compuertas verificadas");
 				}
+				LOGGER.log(Level.FINE,"4");
 				break;
 			default:
 				break;
---
+++ CompuertasAgua/src/base/ControlCompuertasTest.java
@@ -0,0 +1,17 @@
+package base;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+
+class ControlCompuertasTest {
+
+	@ParameterizedTest
+	
+	@Test
+	void test() {
+		
+	}
+
+}
